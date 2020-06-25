f72b0ba5f1fb5d3a618a271093d7c88df439df7f
Jan Gutt <jangutt@trdlnk.com>
2020-06-25 15:50:44 +0200

    ...

--- ambient.lean
+++ ambient.lean
@@ -22,10 +22,10 @@ class ambient_module (M : Type) [lie_ring M] [lie_algebra ℤ M] :=
 (invol_sl2 : τ E = -F ∧ τ H = -H ∧ τ F = -E)
 (invol_circle : ∀ (i : ℤ), τ ∘ (z i) = z (-i) ∘ τ)
 (invol_shift : τ ∘ σ = σ ∘ τ)
-(sl2_shift : ∀ (m : M) (i j : ℤ), 
-               ⁅(z i) E, (z j) (σ m)⁆ = 0
-             ∧ ⁅(z i) H, (z j) (σ m)⁆ = 0 
-             ∧ ⁅(z i) F, (z j) (σ m)⁆ = 0)
+(sl2_shift : ∀ (m : M) (i : ℤ), 
+               ⁅(z i) E, σ m⁆ = 0
+             ∧ ⁅(z i) H, σ m⁆ = 0 
+             ∧ ⁅(z i) F, σ m⁆ = 0)
 (circle_shift : ∀ (m m' : M) (i j : ℤ), ⁅(z i) m, (z j) (σ m')⁆ = (z i) ⁅m, (z j) (σ m')⁆)
 (sl2_circle : ∀ (i j : ℤ) (m : M), 
                 ⁅(z i) E, (z j) m⁆ = j • (z (i+j+1)) m + (z j) ⁅(z i) E, m⁆ 
@@ -87,37 +87,13 @@ end
 
 end lift
 
-def interpret_gen (ζ : M) : gen → M
-| gen.A  := ζ
-| gen.A' := τ ζ
-
 def interpret_sl2_gen : gen → M
 | gen.A  := E
 | gen.A' := -F
 
-def interpret (ζ : M) : words → M := lift (interpret_gen  ζ)
-
 def interpret_sl2 : words → M := lift interpret_sl2_gen
 
-lemma interpret_ze : ∀ (ζ : M) (a : gen), interpret ζ (words.of a) = interpret_gen ζ a :=
-begin intros, unfold interpret, simp [lift.ze] end
-
-lemma interpret_su : ∀ (ζ : M) (a : gen) (b : words)
-                   , interpret ζ (words.of a * b) = ⁅interpret_gen ζ a, interpret ζ b⁆ :=
-begin intros, unfold interpret, simp [lift.su] end
-
-lemma interpret_gen_invol : ∀ (ζ : M) (a : gen)
-                          , interpret_gen ζ (gen.τ a) = τ (interpret_gen ζ a) :=
-begin
-    intros, cases a, simpa [interpret_gen], rw (gen.τ), simp [interpret_gen],
-    have h := str_invol,
-    simp [function.comp] at h,
-    apply_fun (λ (f : M → M), f ζ) at h,
-    rw h
-end 
-
-lemma interpret_sl2_gen_invol : ∀ (a : gen)
-                              , interpret_sl2_gen (gen.τ a) = @id M (τ $ interpret_sl2_gen a) :=
+lemma interpret_sl2_gen_invol : ∀ (a : gen), interpret_sl2_gen (gen.τ a) = τ (interpret_sl2_gen a : M) :=
 begin
     intros, cases a, 
     unfold gen.τ, unfold interpret_sl2_gen, simp [invol_sl2],
@@ -127,38 +103,89 @@ begin
     exact (linear_map.map_neg (τ).to_linear_map x)
 end 
 
-lemma interpret_invol : ∀ (ζ : M) (w : words), interpret ζ (invol.invol w) = invol.invol (interpret ζ w) :=
-begin
-    intros, unfold interpret, unfold invol.invol, 
-    exact (lift.invol (interpret_gen ζ) (interpret_gen_invol ζ) w)
-end
-
-lemma interpret_sl2_invol : ∀ (w : words), interpret_sl2 (invol.invol w) = @id M (invol.invol $ interpret_sl2 w) :=
+lemma interpret_sl2_invol : ∀ (w : words), interpret_sl2 (invol.invol w) = invol.invol (interpret_sl2 w : M) :=
 begin
     intros, unfold interpret_sl2, unfold invol.invol, 
     exact (lift.invol interpret_sl2_gen interpret_sl2_gen_invol w)
 end
 
-def serpentine (ζ : M) : Prop := (ζ = E + (z 1) (σ (ζ - H))) 
-
-def serpentine_str_H (ζ : M) (hζ : serpentine ζ) : ⁅ H, ζ ⁆ = ζ :=
+-- useful in rewriting expressions
+lemma deriv (x : M) : ∀ (y z : M), ⁅x, ⁅y, z⁆⁆ = ⁅⁅x, y⁆, z⁆ + ⁅y, ⁅x, z⁆⁆ :=
 begin
-  sorry
+    intros,
+    rw ←(lie_skew x z), rw lie_neg,
+    rw ←(lie_skew ⁅x, y⁆ z),
+    rw add_comm,
+    have h : ∀ (a b c : M), a + b + c = 0 → a = -b + -c := by sorry,
+    apply h, exact lie_ring.jacobi x y z,
+end  
+
+section zeta 
+
+variable (ζ : M)
+
+def interpret_gen : gen → M
+| gen.A  := ζ
+| gen.A' := τ ζ
+
+def interpret  : words → M := lift (interpret_gen  ζ)
+
+lemma interpret_ze : ∀  (a : gen), interpret ζ (words.of a) = interpret_gen ζ a :=
+begin intros, unfold interpret, simp [lift.ze] end
+
+lemma interpret_su : ∀  (a : gen) (b : words)
+                   , interpret ζ (words.of a * b) = ⁅interpret_gen ζ a, interpret ζ b⁆ :=
+begin intros, unfold interpret, simp [lift.su] end
+
+lemma interpret_gen_invol : ∀  (a : gen), interpret_gen ζ (gen.τ a) = τ (interpret_gen ζ a) :=
+begin
+    intros, cases a, simpa [interpret_gen], rw (gen.τ), simp [interpret_gen],
+    have h := str_invol,
+    simp [function.comp] at h,
+    apply_fun (λ (f : M → M), f ζ) at h,
+    rw h
+end 
+
+lemma interpret_invol : ∀  (w : words), interpret ζ (invol.invol w) = invol.invol (interpret ζ w) :=
+begin
+    intros, unfold interpret, unfold invol.invol, 
+    exact (lift.invol (interpret_gen ζ) (interpret_gen_invol ζ) w)
+end
+
+def serpentine : Prop := ζ = E + (z 1) (σ (ζ - H))
+
+def serpentine_str_H  (hζ : serpentine ζ) : ⁅ H, ζ ⁆ = ζ :=
+begin
+    unfold serpentine at hζ,
+    rw hζ,
+    simp [lie_add, str_sl2],
+    have h : (H : M) = (z 0) H := by simp [str_circle.1],
+    rw h, 
+    simp [sl2_circle],
+    simp [(sl2_shift _ 0).2]
+end
+
+def serpentine_str_H' (hζ : serpentine ζ) : ⁅ H, τ ζ ⁆ = -(τ ζ) :=
+begin
+    intros,
+    rw (_ : H = -(-H)),
+    rw ←(invol_sl2.2.1),
+    rw neg_lie,
+    erw ←τ.map_lie,
+    rw serpentine_str_H ζ hζ, 
+    simp [coe_fn, has_coe_to_fun.coe],
+    simp
 end
 
 /-
-lemma interpret_wt 
-{M : Type} [lie_ring M] [lie_algebra ℤ M] [ambient_module M] 
-(ζ : M) (hζ : serpentine ℤ ζ) : ∀ (w : words), ⁅ H k, interpret ℤ ζ w ⁆ = w.wt • interpret ℤ ζ w :=
+lemma interpret_wt (hζ : serpentine ζ) : ∀ (w : words), ⁅ H, interpret ζ w ⁆ = w.wt • interpret ζ w :=
 begin
-    intros, unfold interpret, unfold lift, induction w, induction w_snd, 
-    -- base
-    unfold words.rec, unfold words.wt, unfold free_semigroup.rec_on,   
-    unfold words.rec, unfold free_semigroup.rec_on,
-    cases w_fst,
-    unfold interpret_gen, unfold words.wt_gen,
 end
- -/
+-/
+end zeta
+
+
+
 
 end ambient_module
 
