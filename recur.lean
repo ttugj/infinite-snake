d539b647ce3a4258eacad14ff1bef286d363b78a
Jan Gutt <jangutt@trdlnk.com>
2020-08-23 22:08:19 +0200

    remove a few sorrys

--- recur.lean
+++ recur.lean
@@ -86,8 +86,14 @@ begin
             -- case A
             simp [words.μ_ze], simp [interpret_sl2_ze],  unfold interpret_gen, unfold words.wt_gen, unfold interpret_sl2_gen,
             unfold neg_z, conv_lhs { congr, congr, rw hζ }, simp,
-            have h  : (z 1) (σ (ζ - H)) = (z 1) (σ ζ) - (z 1) (σ H) := by sorry,    -- TODO
-            have h' : (z 1) (σ (-H : M))= -((z 1) (σ H)) := by sorry,               -- TODO
+            have h  : (z 1) (σ (ζ - H)) = (z 1) (σ ζ) - (z 1) (σ H) := begin
+                    have k : ∀ (x y : M), σ (x - y) = σ x - σ y := linear_map.map_sub σ.to_linear_map,
+                    simp [k] 
+                end,
+            have h' : (z 1) (σ (-H : M))= -((z 1) (σ H)) := begin
+                    have k : ∀ (x : M), σ (-x) = -(σ x) := linear_map.map_neg σ.to_linear_map,
+                    simp [k],
+                end, 
             have h'': ∀ (a b c : M), a + (b - c) + (-b) - a = -c := begin intros, abel end,
             simp [h], simp [h'], simp [h''],
             -- case A'
